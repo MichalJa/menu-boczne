ebea1744b5935c5d14486fefdebd6a543d9da6b2
MichalJa <96233129+MichalJa@users.noreply.github.com>
2022-01-12 17:03:40 +0100

    menu-boczne

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.hamburger').click(function () {
+        $('.main-menu').toggleClass('open');
+    });
+    $('.main-menu-item a').click(function (event) {
+        event.preventDefault();
+
+        $('body').fadeOut(2000, function () {
+            window.location.href = $(event.target).attr('href');
+        });
+    });
+});
